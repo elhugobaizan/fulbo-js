5236e435dd140e4a9496ad9d08c3eff5187c4e0c
Hugo Baizan <elhugobaizan@gmail.com>
2026-07-01 13:23:06 -0300

    agregados a gitignore

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-8c5d9594'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ieqcckmcim4"
+    "revision": "0.j574o6n6r3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
